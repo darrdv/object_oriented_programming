2bede4f2e10dcec66380bc0e52e13a6ef90129bf
Darren.DV <darr.dv@gmail.com>
2016-08-11 19:17:16 -0400

    Here's Paperboy class

--- exercise_2/paperboy.rb
+++ exercise_2/paperboy.rb
@@ -1,12 +1,11 @@
-class Paperboy()
-  #attr_accessor quota
+class Paperboy
 
   def initialize(name, side)
     @name = name
     @side = side
     @experience = 0 #num of papers delivered
-    @minimum = 50
     @earnings = 0
+    @minimum = 50
     @quota = 0
   end
 
@@ -14,27 +13,33 @@ class Paperboy()
     @quota
   end
 
-  def quota(experience)
+  def get_quota(experience)
      @minimum + experience/2
   end
 
   def deliver(start_address, end_address)
-    @experience = (end_address - start_address)/ 2
-    @quota = quota(@experience)
-    @earnings = 0.25* @experience
+    homes_delivered = (end_address - start_address)/ 2
+
+    #allowing for start_address being larger than end_address
+    @experience = homes_delivered.abs
+
+    @quota = get_quota(@experience)
+    @earnings = 0.25 * @experience
 
     if @experience > @quota
       difference = @experience - @quota
       over_quota = difference * 0.50
       @earnings = @earnings + over_quota
     elsif @experience < @quota
-      @earnings = @earnings - $2.00
+      @earnings = @earnings - 2
     end
   end
 
-  def report
-    "I'm #{@name}, I've delivered #{@experience} papers and I've earned
-    $#{@earnings} dollars so far."
+  def earnings
+    @earnings
   end
 
+  def report
+    "I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} dollars so far."
+  end
 end
