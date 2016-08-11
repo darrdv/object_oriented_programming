79d3ca7d01ef99aab4557e3a679c1e7a8ba7f319
Darren.DV <darr.dv@gmail.com>
2016-08-11 16:22:46 -0400

    In progress

---
+++ exercise_2/paperboy.rb
@@ -0,0 +1,20 @@
+class Paperboy()
+  def initialize(name, side)
+    @name = name
+    @side = side
+    @quota = 50 #min quota
+  end
+
+  def quota
+
+  end
+
+  def deliver(start_address, end_address)
+
+  end
+
+  def report
+
+  end
+
+end
